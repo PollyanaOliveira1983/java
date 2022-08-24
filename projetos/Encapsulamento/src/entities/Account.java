934ba75dc0c6eebcfa9a33938cd52a8ef959131a
Pollyana <pollyana.oliveira@gmail.com>
2022-08-24 16:47:37 -0300

    atualização encapsulamento

---
+++ projetos/Encapsulamento/src/entities/Account.java
@@ -0,0 +1,55 @@
+package entities;
+
+public class Account {
+	
+	private int number;
+	private String holder;
+	private double balance;
+	
+	//Constructor com deposito inicial
+	public Account(int number, String holder, double initialDeposit) {
+		this.number = number;
+		this.holder = holder;
+		deposit(initialDeposit);
+	}
+	
+	//Constructor sem deposito inicial
+	public Account(int number, String holder) {
+		this.number = number;
+		this.holder = holder;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+		
+	public void deposit(double amount) {
+		balance = amount + balance;
+	}
+	
+	public void withdraw(double amount) {
+		balance = balance - amount - 5.00; 
+	}
+	
+	public String toString() {
+		return "Account "
+				+ number
+				+ ", Holder: "
+				+ holder
+				+ ", Balance: $ "
+				+ String.format("%.2f", balance);
+	}
+	
+}
