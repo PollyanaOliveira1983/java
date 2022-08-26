15201a37a3d81eeae96c16f050aefa40d06c4bf5
Pollyana <pollyana.oliveira@gmail.com>
2022-08-26 17:33:12 -0300

    finalização memoria, vetores e listas

---
+++ projetos/MemoriaVetoresListas/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String[] vect = new String[] {"Maria", "Bob", "ALex"};
+		
+		for(int i=0; i<vect.length; i++) {
+			System.out.println(vect[i]);
+		}
+		
+		System.out.println("--------------------------------------");
+		//laco for each
+		for(String obj : vect) {
+			System.out.println(obj);
+		}
+		sc.close();
+	}
+
+}
---
+++ projetos/MemoriaVetoresListas/src/application/ProgramSomaVetor.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramSomaVetor {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n;
+		double soma, media;
+		
+		System.out.print("Quantos numeros voce vai digitar? ");
+		n = sc.nextInt();
+		
+		double[] vect = new double[n];
+				
+		for(int i=0; i<n; i++) {
+			System.out.print("Digite um numero: ");
+			vect[i] = sc.nextDouble();
+		}
+		
+		soma = 0;
+		for(int i=0; i<n; i++) {
+			soma += vect[i];
+		}
+		
+		media = soma / n;
+		
+		System.out.printf("VALORES =  ");
+		for(int i=0; i<n; i++) {
+			System.out.printf("%.1f  ", vect[i]);
+		}
+		
+		System.out.printf("\nSOMA = %.2f\n", soma);
+	    System.out.printf("MEDIA = %.2f\n", media);
+	
+	    sc.close();
+	}
+
+}
