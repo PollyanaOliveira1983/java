a04b4bb078579c333efdca59cdf14f1e0743b704
Pollyana <pollyana.oliveira@gmail.com>
2022-09-05 16:09:26 -0300

    atualização nome

--- projetos/TratamentoDeExcecoes/src/application/ProgramSolucao.java
+++ projetos/TratamentoDeExcecoes/src/application/ProgramSolucao.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 import model.entities.Reservation;
 
-public class ProgramSolucaoRuim {
+public class ProgramSolucao {
 
 	public static void main(String[] args) throws ParseException {
 		
