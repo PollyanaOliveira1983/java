c1d4e0cfc8c77f5a90ea9a019cd13ef75781e3f1
Pollyana <pollyana.oliveira@gmail.com>
2022-09-05 16:27:02 -0300

    Bad Solution

--- projetos/TratamentoDeExcecoes/src/application/ProgramSolucao.java
+++ projetos/TratamentoDeExcecoes/src/application/ProgramSolucao.java
@@ -3,7 +3,6 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 import java.util.Scanner;
 
 import model.entities.Reservation;
@@ -37,15 +36,11 @@ public class ProgramSolucao {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: "+ error);
 			}
 			else {
-				reservation.updateDates(checkIn, checkOut);
 				System.out.println("Reservation: " + reservation);
 			}
 		}
